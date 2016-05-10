79244e5e3b2ffddf3841ebca6788056a7f67b82f
Andrew Appleton <andysapple@gmail.com>
2016-05-10 10:14:10 +0100

    Fix tests

--- lib/serializers/todo.rb
+++ lib/serializers/todo.rb
@@ -4,7 +4,7 @@ class Serializers::Todo < Serializers::Base
       id:         todo.id,
       created_at: todo.created_at.iso8601,
       updated_at: todo.updated_at.iso8601,
-      name:      todo.title,
+      name:       todo.title,
       completed:  todo.completed,
       links: {
         user: todo.user_id
